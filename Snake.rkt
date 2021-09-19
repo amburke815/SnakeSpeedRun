9b0b1ba9c0f3877c1568815c53cc9dd96c23d91a
amburke815 <amburke815@gmail.com>
2021-09-18 22:25:13 -0400

    updated 2 data defs

--- Snake.rkt
+++ Snake.rkt
@@ -3,6 +3,7 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Snake) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; SNAKE ;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; GAME DEF ;;;
 
@@ -12,6 +13,27 @@
 ;; - Difficulty
 ;; - Fruit
 
-;; (make-snake-game s p t d f) := A SnakeGame with a Snake _s_, a
+;; (make-snake-game s p t d f) := A SnakeGame with a Snake _s_, a current time _t_, and a Fruit _f_
 (define-struct snake-game (snake time difficulty fruit))
-cat S
+
+;; snake-game-template :: SnakeGame -> ???
+#;(define (snake-game-template sg)
+    (... (snake-template (snake-game-snake sg))
+         (snake-game-time sg)
+         (snake-game-difficulty sg)
+         (fruit-template (snake-game-fruit sg)) ...))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; SNAKE DEF ;;;
+
+;; A Snake consists of a
+;; - [List-of SnakePart] -> One of:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; - (cons SnakePart [List-of SnakePart])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; - '()
+;; - Direction
+
+
+;; (make-snake losp d) := A Snake consisting of a [List-of SnakePart] _losp_ and with a head pointing
+;; in the Direction _d_
+ 
